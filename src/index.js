18f13b2931af3fa9bea7b81ac978456d9bd6bf12
MiltonTomasino <jonathantomasino13@gmail.com>
2025-05-03 20:12:40 -0700

    Create Functioning Modal For Creating New Project
    This connects button to the form and shows the user a modal box so that
    they can begin to input their information.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,56 @@
 import "./style.css";
 
+class Project {
+    constructor(title, description, dueDate, priority) {
+        this.title = title;
+        this.description = description;
+        this.dueDate = dueDate;
+        this.priority = priority;
+    }
+}
+
+const ProjectManipulation = (() => {
+    function createNewProject() {
+
+    }
+
+    return {
+        createNewProject,
+    }
+})();
+
+
+const DomManipulation = (() => {
+    let modal = document.querySelector(".modal");
+
+
+    function showFormModal() {
+        modal.showModal();
+    }
+
+    function closeFormModal() {
+        modal.close();
+    }
+
+    return {
+        showFormModal,
+        closeFormModal,
+    }
+})();
+
+const ButtonManipulation = (() => {
+    let newProject = document.querySelector("#new-project");
+
+    newProject.addEventListener("click", () => {
+        DomManipulation.showFormModal();
+        ProjectManipulation.createNewProject();
+    });
+
+    let closeModal = document.querySelector("#close-modal");
+
+    closeModal.addEventListener("click", () => DomManipulation.closeFormModal());
+
+})();
+
+
 console.log("Hello from index.js!");
