1c2e5e67e4468edd76e338ba39b299e528dae7ee
MiltonTomasino <jonathantomasino13@gmail.com>
2025-05-04 21:04:11 -0700

    Create Task Checkbox Functionality to Each Card
    The user can now input individual tasks that need to be done
    individually in each project. They can check them in the main page to
    signifiy whether they are finished or not.

--- src/index.js
+++ src/index.js
@@ -1,11 +1,12 @@
 import "./style.css";
 
 class Project {
-    constructor(title, description, dueDate, priority) {
+    constructor(title, description, dueDate, priority, tasks = []) {
         this.title = title;
         this.description = description;
         this.dueDate = dueDate;
         this.priority = priority;
+        this.tasks = tasks;
     }
 }
 
@@ -63,13 +64,16 @@ const localStorageManipulation = (() => {
 
 const ProjectManipulation = (() => {
 
-    function createProject(title, description, dueDate, priority) {
-        let project = new Project(title, description, dueDate, priority);
+    function createProject(title, description, dueDate, priority, tasks) {
+        let project = new Project(title, description, dueDate, priority, tasks);
         localStorageManipulation.appendToList(project);
         return project;
     }
 
     function createNewCard(project) {
+
+        if (project.tasks.length > 0) console.log("tasks has more than 1");
+        
 
         let card = document.createElement("div");
         card.classList.add("card");
@@ -82,6 +86,28 @@ const ProjectManipulation = (() => {
         due.classList.add("due-date")
         let prio = document.createElement("p"); 
         prio.classList.add("priority")
+
+        let taskBox = document.createElement("div");
+        taskBox.classList.add("task-box");
+
+        if (project.tasks.length > 0) {
+            for (let i = 0; i < project.tasks.length; i++) {
+                console.log(project.tasks[i]);
+                let taskContainer = document.createElement("div");
+
+                taskContainer.classList.add("task-container");
+                let task = document.createElement("label");
+                let taskCheck = document.createElement("input");
+
+                taskCheck.type = "checkbox";
+                taskCheck.value = false;
+                task.textContent = project.tasks[i];
+
+                taskContainer.appendChild(task);
+                taskContainer.appendChild(taskCheck);
+                taskBox.appendChild(taskContainer);
+            }
+        }
         
         header1.textContent = `${project.title}`;
         para.textContent = `${project.description}`;
@@ -92,13 +118,28 @@ const ProjectManipulation = (() => {
         card.appendChild(para);
         card.appendChild(due);
         card.appendChild(prio);
+        card.appendChild(taskBox);
 
         return card;
+    }
+
+    function getAllTasks() {
+        let tasks = document.querySelectorAll(".task");
+        let taskList = [];
+
+        if (tasks.length < 1) return taskList;
+
+        for (let i = 0; i < tasks.length; i++) {
+            taskList.push(tasks[i].value);
+        }
+
+        return taskList;
     }
 
     return {
         createNewCard,
         createProject,
+        getAllTasks,
     }
 })();
 
@@ -106,6 +147,7 @@ const ProjectManipulation = (() => {
 const DomManipulation = (() => {
     let modal = document.querySelector(".modal");
     let mainContent = document.querySelector(".main");
+    let gridTask = document.querySelector(".grid-task");
 
     function appendToMain(child) {
         mainContent.appendChild(child);
@@ -127,9 +169,27 @@ const DomManipulation = (() => {
         let list = localStorageManipulation.getProjectList();
         if (list.length < 1) return;
 
-        for (let i = 0; i < list.length; i++) {
+        for (let i = list.length - 1; i >= 0; i--) {
             appendToMain(ProjectManipulation.createNewCard(list[i]));
         }
+    }
+
+    function addTask() {
+        let task = document.createElement("div");
+        let taskLabel = document.createElement("label");
+        let taskInput = document.createElement("input");
+
+        task.classList.add("task-element");
+
+        taskInput.type = "text";
+        taskInput.classList.add("task");
+
+        taskLabel.textContent = "Task: ";
+
+        task.appendChild(taskLabel);
+        task.appendChild(taskInput);
+
+        gridTask.appendChild(task);
     }
 
     return {
@@ -137,6 +197,7 @@ const DomManipulation = (() => {
         closeFormModal,
         appendToMain,
         updateDOM,
+        addTask,
     }
 })();
 
@@ -162,15 +223,24 @@ const ButtonManipulation = (() => {
         let projectDueDate = document.querySelector("#due-date");
         let projectPriority = document.querySelector("#project-priority");
 
-        let project = ProjectManipulation.createProject(
+        let tasks = ProjectManipulation.getAllTasks();
+
+        ProjectManipulation.createProject(
             projectName.value,
             projectDesc.value,
             projectDueDate.value, 
-            projectPriority.value);
+            projectPriority.value,
+            tasks);
 
         DomManipulation.updateDOM();
         DomManipulation.closeFormModal();
     })
+
+    let taskBtn = document.querySelector("#create-task");
+    taskBtn.addEventListener("click", (e) => {
+        e.preventDefault();
+        DomManipulation.addTask();
+    });
 
 })();
 
